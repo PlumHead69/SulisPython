79d387faae1130885139db8b2fe32157a16fd809
PlumHead69 <lenardf.horvath@gmail.com>
2024-09-07 08:34:54 +0200

    finished

--- PythonFeladatok/adatokbeolvasasa.py
+++ PythonFeladatok/adatokbeolvasasa.py
@@ -1,17 +1,26 @@
 nums = input("Add meg a számokat: ")
 
+result = tuple(nums)
+
 def Main(nums):
 
-    nums.strip()
+
     numlist = []
+    paros=0
+    paratlan=0
 
     for i in nums:
-        numlist.append(int(i))
+        if i != ",":
+            numlist.append(int(i))
+
 
     for k in numlist:
-        if k//2 != 0:
-            numlist.remove(k)
+        if k%2 != 0:
+            paratlan+=1
+        else:
+            paros+=1
 
-    print(numlist)
-#gyerunk
+    print("Paros szamok száma " + str(paros))
+    print("Páratlan számok száma " + str(paratlan))
+
 Main(nums)
